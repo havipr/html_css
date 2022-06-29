1abb3c05c67d64b90f37ab7f98a217c97c94e6db
Javi <javierreinapinzon@gmail.com>
2022-06-29 17:38:21 +1000

    all HTML practice to be send to GitHub

---
+++ CSS/havi_reina/public/scripts/data.js
@@ -0,0 +1,59 @@
+const data = {
+  code: [
+    {
+      title: "COMING SOON",
+      img: ["public/assets/dev/elon.jpeg", 
+          ],
+      stack: [
+        "",
+      ],
+      roles: [""],
+      desc:
+        '',
+      store: "",
+      contact: ""
+    },
+  ],
+
+  design: [
+    {
+      title: "REINA FOUNDATION",
+      img: [
+        "public/assets/design/fundpic.jpg",
+      ],
+      stack: ["Zipaquira, Colombia","","",""],
+      roles: ["Founder"],
+      desc:
+        '<strong> Changing Lives In Colombia. </strong> <br> <br> We are on a mission, to close the knowledge gap in Colombia. We believe giving is not just about making a donation. It is about making a difference.',
+      blog: "https://blog.javierreina.com",
+      suscribe: "https://blog.javierreina.com/#subscribe"
+    },
+
+  ],
+  emojis: [
+    "🏳️‍🌈",
+    "🌏",
+    "🇯🇵",
+    "👽",
+    "🇨🇴",
+    "🇨🇦",
+    "🇧🇷",
+    "🇪🇺",
+    "😈",
+    "😇",
+    "🇺🇸",
+    "🇦🇺",
+    "🇿🇦",
+    "🇨🇳",
+    "🇲🇽",
+    "💻",
+    "👨‍⚕️",
+    "👮🏻‍♀️"
+  ],
+  icons: [
+ 
+  ],
+  favicons: [
+
+  ]
+};
